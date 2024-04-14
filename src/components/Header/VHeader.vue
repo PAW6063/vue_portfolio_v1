b4718cd1a6db8a55fbe509c0aa5d9cffca86b416
Phillip <phillip.welch2@mavs.uta.edu>
2024-04-14 12:39:18 -0500

    Starting structure of the site. I added Header, Logo, Nav, Footer.

---
+++ src/components/Header/VHeader.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import VLogo from './VLogo.vue'
+    import VNavbar from './VNavbar.vue'
+</script>
+
+<template>
+    <VLogo>
+
+    </VLogo>
+    <VNavbar>
+        
+    </VNavbar>
+</template>
